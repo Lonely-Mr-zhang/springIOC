8171501c8a3a6a93b33126bacb1caedcd94722c6
Lonely-Mr-zhang <2352794560@qq.com>
2020-06-14 14:48:15 +0800

    springIOC基于xml的环境

--- src/main/java/io/github/lonelyMrZhang/ui/Client.java
+++ src/main/java/io/github/lonelyMrZhang/ui/Client.java
@@ -1,8 +1,11 @@
 package io.github.lonelyMrZhang.ui;
 
-import io.github.lonelyMrZhang.factory.BeanFactory;
+import io.github.lonelyMrZhang.dao.IAccountDao;
+//import io.github.lonelyMrZhang.factory.BeanFactory;
 import io.github.lonelyMrZhang.service.IAccountService;
 import io.github.lonelyMrZhang.service.impl.AccountServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @description: 模拟表现层，调用业务层
@@ -10,8 +13,19 @@ import io.github.lonelyMrZhang.service.impl.AccountServiceImpl;
  * @date: 2020/6/12 12:53 上午
  */
 public class Client {
+
+    /**
+     * 获取Spring的IOC核心容器，并根据id获取对象
+     * @param args
+     */
     public static void main(String[] args) {
-        IAccountService accountService = new AccountServiceImpl();
-        accountService.saveAccount();
+        //1、获取核心容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        //2、根据id获取对象
+        IAccountService accountService = (IAccountService)ac.getBean("accountService");
+        IAccountDao accountDao =  ac.getBean("accountDao", IAccountDao.class);
+
+        System.out.println(accountService);
+        System.out.println(accountService);
     }
 }
